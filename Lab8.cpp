b2dba110c6ed9f520250fedc2649ec0740d815e5
KovtunIKM724a <Yevhen.Kovtun@infiz.khpi.edu.ua>
2025-03-07 10:26:40 +0200

    Lab8
    added

---
+++ Lab8.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const int n = 15;
+
+int main()
+{
+	setlocale(LC_CTYPE, "ukr");
+
+	double array[n];
+	float max_neg;
+	int a;
+
+	for (int i = 0; i < n; i++)
+	{
+		double first_N = rand() % 201 - 100;
+		double second_N = (double)(rand() % 2000 - 1000) / 1000;
+
+		array[i] = first_N + second_N;
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << array[i] << " ";
+	}
+	cout << endl;
+
+	max_neg = -100;
+	for (int i = 0; i < n; i++)
+	{
+		if (array[i]<0 && array[i]>max_neg) {
+			max_neg = array[i];
+		}
+	}
+	a = round(max_neg);
+
+	cout << a;
+}
